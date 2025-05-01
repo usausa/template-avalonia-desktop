09555b0cdc54b66d803fa9bddb64c5313b060f64
usausa <machi.pon@gmail.com>
2025-05-01 17:29:44 +0900

    Update baseline

--- Template.DesktopApp/Program.cs
+++ Template.DesktopApp/Program.cs
@@ -10,11 +10,11 @@ public static class Program
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
     [STAThread]
-    public static int Main(string[] args) => BuildAvaloniaApp()
+    public static int Main(string[] args) => BuildAppBuilder()
         .StartWithClassicDesktopLifetime(args);
 
     // Avalonia configuration, don't remove; also used by visual designer.
-    public static AppBuilder BuildAvaloniaApp() =>
+    public static AppBuilder BuildAppBuilder() =>
         AppBuilder.Configure<App>()
             .UsePlatformDetect()
             .WithInterFont()
